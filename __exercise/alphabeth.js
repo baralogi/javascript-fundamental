a600fa52413bb7b63dc8d321a0e7e93bf61ba39f
Sebastianus Sembara <sembara9090@gmail.com>
2021-11-06 07:25:31 +0700

    add another exercise

---
+++ __exercise/alphabeth.js
@@ -0,0 +1,3 @@
+let alphabet = String.fromCharCode(...Array(123).keys()).slice(97);
+
+console.log(alphabet);
---
+++ __exercise/square.js
@@ -0,0 +1,14 @@
+let rows = 5
+let column = 4
+let pattern = ''
+
+for (let i = 0; i < rows; i++) {
+    pattern += '\n';
+    for (let j = 0; j < column; j++) {
+        
+        pattern += '*';
+        
+    }
+}
+
+console.log(pattern);
